b44e0cfa481cca11c627661a8e7316ffb797d65d
shiminga <1056927138@qq.com>
2020-10-12 22:55:07 +0800

    commit code

---
+++ cloud-provider-payment8001/src/main/java/com/cloud/payment/entities/CommonResult.java
@@ -0,0 +1,20 @@
+package com.cloud.payment.entities;
+
+public class CommonResult<T> {
+
+    private Integer code;
+
+    private String message;
+
+    private T data;
+
+    public CommonResult(Integer code, String message) {
+        this(code,message,null);
+    }
+
+    public CommonResult(Integer code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+}
